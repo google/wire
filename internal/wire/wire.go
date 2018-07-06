e1df90c68f1fe72a13f7724c0e6bd3ac9f8766bc
Ross Light <light@google.com>
2018-07-06 12:49:48 -0700

    wire: only apply wireinject build tag to generated package (google/go-cloud#176)

--- internal/wire/wire.go
+++ internal/wire/wire.go
@@ -45,16 +45,26 @@ func Generate(bctx *build.Context, wd string, pkg string) ([]byte, []error) {
 	}
 	// TODO(light): Stop errors from printing to stderr.
 	conf := &loader.Config{
-		Build: new(build.Context),
+		Build: bctx,
 		Cwd:   wd,
 		TypeCheckFuncBodies: func(path string) bool {
 			return path == mainPkg.ImportPath
 		},
+		FindPackage: func(bctx *build.Context, importPath, fromDir string, mode build.ImportMode) (*build.Package, error) {
+			if importPath == mainPkg.ImportPath {
+				// Load in the generated package with the wireinject build tag
+				// to pick up the injector template. The imported packages
+				// should be imported as normal. Since the *build.Context is
+				// shared between calls to FindPackage, this uses a copy.
+				bctx2 := new(build.Context)
+				*bctx2 = *bctx
+				n := len(bctx2.BuildTags)
+				bctx2.BuildTags = append(bctx2.BuildTags[:n:n], "wireinject")
+				bctx = bctx2
+			}
+			return bctx.Import(importPath, fromDir, mode)
+		},
 	}
-	*conf.Build = *bctx
-	n := len(conf.Build.BuildTags)
-	// TODO(light): Only apply wireinject build tag on main package.
-	conf.Build.BuildTags = append(conf.Build.BuildTags[:n:n], "wireinject")
 	conf.Import(pkg)
 
 	prog, err := conf.Load()
