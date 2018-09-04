46248162d7367fbaa74961a1edd9af8544198286
Robert van Gent <rvangent@google.com>
2018-09-04 09:10:14 -0700

    wire: add a testcase with multiple similar packages (google/go-cloud#387)
    This is based on the description in google/go-cloud#372. It will be useful if we add wire.FieldsOf, and also for the disambiguation-using-package.
    
    Updates google/go-cloud#372
    Updates google/go-cloud#386

---
+++ internal/wire/testdata/MultipleSimilarPackages/main/wire.go
@@ -0,0 +1,62 @@
+// Copyright 2018 The Go Cloud Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//+build wireinject
+
+package main
+
+import (
+	"fmt"
+
+	"example.com/bar"
+	"example.com/baz"
+	"example.com/foo"
+	"github.com/google/go-cloud/wire"
+)
+
+type MainConfig struct {
+	Foo *foo.Config
+	Bar *bar.Config
+	Baz *baz.Config
+}
+
+type MainService struct {
+	Foo *foo.Service
+	Bar *bar.Service
+	Baz *baz.Service
+}
+
+func (m *MainService) String() string {
+	return fmt.Sprintf("%d %d %d", m.Foo.Cfg.V, m.Bar.Cfg.V, m.Baz.Cfg.V)
+}
+
+func newMainService(*foo.Config, *bar.Config, *baz.Config) *MainService {
+	wire.Build(
+		MainService{},
+		foo.New,
+		bar.New,
+		baz.New,
+	)
+	return nil
+}
+
+func main() {
+	cfg := &MainConfig{
+		Foo: &foo.Config{1},
+		Bar: &bar.Config{2},
+		Baz: &baz.Config{3},
+	}
+	svc := newMainService(cfg.Foo, cfg.Bar, cfg.Baz)
+	fmt.Println(svc.String())
+}
