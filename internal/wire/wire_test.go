c3ea2961cbe8299d1a79eb7dc3f42675d595a49a
Ross Light <light@google.com>
2018-11-07 14:11:44 -0800

    wire: in tests, don't copy want/wire_gen.go into the temporary GOPATH (google/go-cloud#646)
    This does not change the results of the test, but should not be
    happening.

--- internal/wire/wire_test.go
+++ internal/wire/wire_test.go
@@ -488,12 +488,16 @@ func loadTestCase(root string, wireGoSrc []byte) (*testCase, error) {
 		if err != nil {
 			return err
 		}
-		if !info.Mode().IsRegular() || filepath.Ext(src) != ".go" {
-			return nil
-		}
 		rel, err := filepath.Rel(root, src)
 		if err != nil {
 			return err // unlikely
+		}
+		if info.Mode().IsDir() && rel == "want" {
+			// The "want" directory should not be included in goFiles.
+			return filepath.SkipDir
+		}
+		if !info.Mode().IsRegular() || filepath.Ext(src) != ".go" {
+			return nil
 		}
 		data, err := ioutil.ReadFile(src)
 		if err != nil {
