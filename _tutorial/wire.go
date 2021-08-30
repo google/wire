a4d1de965789579a1ebe1bec293627f872bda727
Robert van Gent <rvangent@google.com>
2021-08-30 13:15:15 -0700

    all: try to fix test workflow

--- _tutorial/wire.go
+++ _tutorial/wire.go
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//+build wireinject
+//go:build wireinject
+// +build wireinject
 
 // The build tag makes sure the stub is not built in the final build.
 package main
